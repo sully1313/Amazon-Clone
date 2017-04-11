a878867e34ae3aaa5cfa7e43ae31d5ec794bb49a
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-10 19:32:07 -0700

    connected mongolab db to app

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://sully1313:fumaboy12@ds159180.mlab.com:59180/amazon13', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
 
 // Middleware
 app.use(morgan('dev'));
