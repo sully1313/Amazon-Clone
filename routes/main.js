a742c930843a93f23c575967360d8e9c2827cd61
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-11 03:59:11 -0700

    configuring signup route

---
+++ routes/main.js
@@ -0,0 +1,17 @@
+var router = require('express').Router();
+
+
+
+
+// homepage Route
+router.get('/', function(req, res) {
+  res.render('main/home');
+});
+
+// about page Route
+router.get('/about', function(req, res) {
+  res.render('main/about');
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 
+
 var User = require('./models/user');
 
 var app = express();
@@ -19,37 +20,19 @@ mongoose.connect('mongodb://root:abc123@ds159180.mlab.com:59180/amazon13', funct
 });
 
 // Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
-// Create User Route
-app.post('/create-user',function(req, res, next) {
-  var user = new User();
+app.use(mainRoutes);
+app.use(userRoutes);
 
-  user.profile.name = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-
-  user.save(function(err) {
-    if (err) return next(err);
-    res.json('Successfully created a new user');
-  });
-});
-
-// homepage Route
-app.get('/', function(req, res) {
-  res.render('main/home');
-})
-
-// about page Route
-app.get('/about', function(req, res) {
-  res.render('main/about');
-});
 
 
 // Server listening on port 3000
