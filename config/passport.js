c14434fe56eabb1c5595477c47dca2723397578c
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-11 19:28:33 -0700

    passport serialize & deserialize & passport middleware

--- config/passport.js
+++ config/passport.js
@@ -14,9 +14,24 @@ passport.deserializeUser(function(id, done) {
 
 
 
-// Middleware
+// Passport Middleware
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  User.findOne({ email: email}, function(err, user) {
+    if (err) return done(err);
 
-
+    if (!user) {
+      return done(null, false, req.flash('loginMessage', 'No user has been found'));
+    }
+    if (!user.comparePassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Oops wrong password pal'));
+    }
+    return done(null, user);
+  });
+}));
 
 
 // custom function to validate
