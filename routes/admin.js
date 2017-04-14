9f3d1332e83f05e21bb9a48cd3c37c3f275cb2bb
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-13 19:34:49 -0700

    admin add category route implemented

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 var router = require('express').Router();
 var Category = require('../models/category');
 
-
+// admin get route to add category
 router.get('/add-category', function(req, res, next) {
   res.render('admin/add-category', { message: req.flash('success')});
 });
 
+// admin post route to create new category
 router.post('/add-category', function(req, res, next) {
   var category = new Category();
   category.name = req.body.name;
@@ -16,3 +17,6 @@ router.post('/add-category', function(req, res, next) {
     return res.redirect('/add-category');
   });
 });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -49,10 +49,11 @@ app.set('view engine', 'ejs');
 
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
+var adminRoutes = require('./routes/admin');
 
 app.use(mainRoutes);
 app.use(userRoutes);
-
+app.use(adminRoutes);
 
 
 // Server listening on port 3000
