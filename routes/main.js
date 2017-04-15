a68374afd99d08f23b839d39493b1599ec7bfcbc
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-14 19:24:30 -0700

    rendered products within categories & added links to the nav

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-
+var Product = require('../models/product');
 
 
 
@@ -13,6 +13,17 @@ router.get('/about', function(req, res) {
   res.render('main/about');
 });
 
-
+//get route renders products for each category
+router.get('/products/:id', function(req, res, next) {
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+});
 
 module.exports = router;
