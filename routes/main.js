c059f0dce1019a29e82fbf280273af1da48082c4
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-15 17:57:28 -0700

    Rendered search results

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+//configure mongoosastic for search
 Product.createMapping(function(err, mapping) {
   if (err) {
     console.log('error creating mapping');
@@ -24,6 +25,28 @@ stream.on('close', function() {
 
 stream.on('error', function(err) {
   console.log(err);
+});
+
+//search routes
+router.post('/search', function(req, res, next) {
+  res.redirect('/search?q=' + req.body.q);
+})
+
+router.get('/search', function(req, res, next) {
+  if (req.query.q) {
+    Product.search({
+      query_string: { query: req.query.q }
+    }, function(err, results) {
+      if (err) return next(err);
+      var data = results.hits.hits.map(function(hit) {
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
 });
 
 // homepage Route
